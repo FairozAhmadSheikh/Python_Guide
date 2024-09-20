0342ee05d29b80a75750c70de8f1def624cbfad9
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-09-20 12:56:26 +0530

    done

--- Admin_Guest.py
+++ Admin_Guest.py
@@ -1,9 +1,9 @@
 Inventory={
-    "Laptop":{"Name":"Lenovo ideapad","Price":2000,"Stock":10},
+    "Laptop":{"Name":"Lenovo ideapad","Price":2000,"Stock":9},
     "Phone":{"Name":"I-Phone","Price":200,"Stock":10}
 }
 # Admin Database and Control
-who_am_i=input("Are You Admin or Guest! \n").lower()
+who_am_i=input("Are You Admin or Customer! \n").lower()
 if who_am_i=="admin":
     password=input("Enter your password : ")
     if password=="Database@123":
@@ -11,40 +11,42 @@ if who_am_i=="admin":
     else:
         print("Wrong Password ")
 else:
+    customer=input("Enter Your Name Please: ") or "Unknown Customer"
+    message="Welcome "+customer
+    print(message.center(30,"="))
     print(" Press 1 to order a Laptop")
-    print(" Print 2 to order a Mobile")
+    print(" Press 2 to order a Mobile")
 
     choice=input(":")
     # Order Laptop
     if choice=="1":
         print("You are Ordering "+Inventory["Laptop"]["Name"]+ ":" +str(Inventory["Laptop"]["Price"])+" RS")
         quantity=int(input("How Many do you want to Order : "))
-        Total_Amount=quantity*int(Inventory["Laptop"]["Price"])
-        print("Your Total Amount for ordering "+ str(quantity) +" "+Inventory["Laptop"]["Name"]+" is : ", Total_Amount)
-        confirmation=input("Enter Yes to confirm order and NO to cancel ").lower()
-        if confirmation=="yes":
-            if quantity<=int(Inventory["Laptop"]["Stock"]):
+        if quantity<=int(Inventory["Laptop"]["Stock"]):
+            Total_Amount=quantity*int(Inventory["Laptop"]["Price"])
+            print("Your Total Amount for ordering "+ str(quantity) +" "+Inventory["Laptop"]["Name"]+" is : ", Total_Amount)
+            confirmation=input("Enter Yes to confirm order and NO to cancel ").lower()
+            if confirmation=="yes":
                 Inventory["Laptop"]["Stock"]-=quantity
                 print("Order Sucessfull"," Stock Left in Our Store is : ",Inventory["Laptop"]["Stock"])
             else:
-                print("We dont Have so much Stock")
+                print("Thanks for Only Having a Look and Missing best Deal on ",Inventory["Laptop"]["Name"])
         else:
-            print("Thanks for Only Having a Look and Missing best Deal on ",Inventory["Laptop"]["Name"])
-
+                print("We dont Have so much Stock You can order max of : ",Inventory["Laptop"]["Stock"])
             # Order Mobile
     elif choice=="2":
         print("You are Ordering "+Inventory["Phone"]["Name"]+ ":" +str(Inventory["Phone"]["Price"])+" RS")
         quantity=int(input("How Many do you want to Order : "))
-        Total_Amount=quantity*int(Inventory["Phone"]["Price"])
-        print("Your Total Amount for ordering "+ str(quantity) +" "+Inventory["Phone"]["Name"]+" is : ", Total_Amount)
-        confirmation=input("Enter Yes to confirm order and NO to cancel ").lower()
-        if confirmation=="yes":
-            if quantity<=int(Inventory["Phone"]["Stock"]):
+        if quantity<=int(Inventory["Phone"]["Stock"]):
+            Total_Amount=quantity*int(Inventory["Phone"]["Price"])
+            print("Your Total Amount for ordering "+ str(quantity) +" "+Inventory["Phone"]["Name"]+" is : ", Total_Amount)
+            confirmation=input("Enter Yes to confirm order and NO to cancel ").lower()
+            if confirmation=="yes":
                 Inventory["Phone"]["Stock"]-=quantity
                 print("Order Sucessfull"," Stock Left in Our Store is : ",Inventory["Phone"]["Stock"])
             else:
-                print("We dont have so much stock left")
+                print("Thanks for Only Having a Look and Missing best Deal on ",Inventory["Phone"]["Name"])
         else:
-            print("Thanks for Only Having a Look and Missing best Deal on ",Inventory["Phone"]["Name"])
+                print("We dont have so much stock left , you can order max of : ",Inventory["Phone"]["Stock"])
     else:
         print("Thanks for visiting our strore")
